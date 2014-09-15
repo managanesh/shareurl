b1f8c173acbbf8c03627a37cc7dd63795877ae15
managanesh <managanesh@gmail.com>
2014-09-15 22:43:20 +0530

    updated tests and entities for @NodeEntity

--- SpringBoot-Neo4j/src/main/java/com/sri/poc/config/ApplicationConfig.java
+++ SpringBoot-Neo4j/src/main/java/com/sri/poc/config/ApplicationConfig.java
@@ -6,20 +6,31 @@ import org.neo4j.kernel.EmbeddedGraphDatabase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.neo4j.config.EnableNeo4jRepositories;
+import org.springframework.data.neo4j.config.Neo4jConfiguration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * Spring JavaConfig configuration class to setup a Spring container and infrastructure components.
  */
 @Configuration
-@ComponentScan
+@ComponentScan(basePackages={"com.sri.poc"})
 @EnableTransactionManagement
-class ApplicationConfig {
+@EnableNeo4jRepositories("com.sri.poc.repo")
 
+class ApplicationConfig extends Neo4jConfiguration {
+
+	public ApplicationConfig(){
+		super.setBasePackage("com.sri.poc");
+	}
+	
 	@Bean(destroyMethod = "shutdown")
 	public GraphDatabaseService graphDatabaseService() {
 		GraphDatabaseFactory f = new GraphDatabaseFactory();
-		
+
 		return f.newEmbeddedDatabase("target/graph.db");
 	}
+
+	
+	 
 }
